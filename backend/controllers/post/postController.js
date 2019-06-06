6c86702719c14adc0b20bec26054782100f73edb
kacperjagiela <jagielakacper80@gmail.com>
2019-06-06 22:28:00 +0200

    created basic registration backend

--- backend/controllers/post/postController.js
+++ backend/controllers/post/postController.js
@@ -1,11 +1,19 @@
 const multer = require("multer");
-const upload = multer();
 const database = require("../../database/database.js");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+const upload = multer();
 const db = new database();
 
 module.exports = app =>{
 	//TODO: verify user in database
 	app.post("/login", (req, res)=>{
+		db.connection.getConnection((err, connection)=>{
+			connection.query(`SELECT * FROM QA_users WHERE username='${req.body.data.username}'`, (err, rows, fields)=>{
+
+			});
+		});
 		res.cookie("login", req.body.data.username, {maxAge:9999999, HttpOnly:false}).send("Logged in");
 	});
 	
@@ -21,12 +29,21 @@ module.exports = app =>{
 	//TODO: handle register
 	app.post("/register", (req, res)=>{
 		db.connection.getConnection((err, connection)=>{
-			connection.query("SELECT * FROM QA_users", (err, rows, fileds)=>{
+			connection.query(`SELECT * FROM QA_users WHERE username='${req.body.data.username}'`, (err, rows, fileds)=>{
 				if(err) throw err;
-				console.log(rows);
+				if(rows[0]){
+					res.send("User exists");
+				}else{
+					bcrypt.genSalt(saltRounds, (err, salt)=>{
+						bcrypt.hash(req.body.data.password, salt, (err, hash)=>{
+							connection.query(`INSERT INTO QA_users (email, username, password, description) 
+							VALUES ('${req.body.data.email}', '${req.body.data.username}', '${hash}', '${req.body.data.description}')`)
+						});
+					});
+					res.send("Created");
+				}
 				connection.release();
 			});
 		});
-		res.send();
 	});
 }
